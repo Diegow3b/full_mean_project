579d78f8cfcc45616fe9ff73538c3548eeeecbaf
Diego Vinicius <diegowebd@hotmail.com>
2017-03-13 15:59:09 -0300

    Configuring Servic

--- client/src/app/tasks/tasks.component.ts
+++ client/src/app/tasks/tasks.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
 
 @Component({
-  selector: 'app-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+    selector: 'app-tasks',
+    templateUrl: './tasks.component.html',
+    styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+    constructor(private taskService: TaskService) {
+        this.taskService.getTasks()
+            .subscribe(tasks => {
+                console.log(tasks);
+            })
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
